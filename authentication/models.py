3ac379e80d3fb6cc79560ea1757d07769649d2af
ranjit damase <ranjitdamase@gmail.com>
2025-10-16 14:08:43 +0545

    Added logout button  ans some changes

--- authentication/models.py
+++ authentication/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
     phone_no=models.CharField(max_length=12,blank=True,null=True)
     location=models.CharField(max_length=100,blank=True,null=True)
     college_name=models.CharField(max_length=150,blank=True,null=True)
--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('register/',views.register_view,name='register'),
     path('login/',views.login_view,name='login'),
-    path('home/',views.home,name='home')
+    path('logout/',views.logout_view,name='logout')
 ]
